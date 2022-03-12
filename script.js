352a94a6c987e658b0d73c01eb6ff448acf121f9
Anant-Agarwal1298 <66722328+Anant-Agarwal1298@users.noreply.github.com>
2022-03-12 23:02:19 +0530

    code added in js file

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("order");
+
+const d = new Date();
+console.log(d);
+
+let getDate = document.getElementById("date");
+let btn1 = document.getElementById("btn");
+let getitemname = document.getElementById("itemname");
+let getseller = document.getElementById("sellername");
+let gettime = document.getElementById("time");
+
+getDate.innerText = d.getDate() + '/' +(d.getMonth()+1) + '/' + d.getFullYear(); 
+console.log(d.getFullYear());
+console.log(d.getDate());
+console.log(d.getMonth()+1);
